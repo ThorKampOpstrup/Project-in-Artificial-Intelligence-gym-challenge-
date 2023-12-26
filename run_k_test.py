fa28bf196f6f6fc2d3a867ddf7393a6664307041
ThotKampOpstrup <thoropstrup@gmail.com>
2023-12-26 23:46:09 +0100

    Automated push: Test for k_test = 100 number 0

--- run_k_test.py
+++ run_k_test.py
@@ -9,17 +9,17 @@ data_path = 'data'
 
 
 # # Run the test for different k values
-subdir = 'k_test_20-100'
+subdir = 'k_test_100-1000'
 base_log_name = 'k_'
 #from 1 to 10 k
 for j in range(number_of_equal_runs):
-    for i in np.arange(20, 101, 10):
+    for i in np.arange(100, 1001, 100):
         k = i
         log_name = base_log_name + str(k) + '_number_' + str(j)
         # Create the command
         print('Running test number ' + str(j)+ ' for k = ' + str(k))
-        # command = [python_interpreter, 'iem-ppo.py', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--re3_k', str(k),'--it', '200', '--n_steps', '1012']
-        # subprocess.run(command)
+        command = [python_interpreter, 'iem-ppo.py', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--re3_k', str(k),'--it', '200', '--n_steps', '2024']
+        subprocess.run(command)
         
         subprocess.run(['git', 'pull'])
         subprocess.run(['git', 'add', '-A'])
