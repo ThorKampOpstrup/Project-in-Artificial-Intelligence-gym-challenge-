c255bb9266d60b69bf3ed3b7cc9ed2bb7bb05526
ThotKampOpstrup <thoropstrup@gmail.com>
2023-12-28 15:20:41 +0100

    Automated push: Test for sigma

--- run_sigma _test.py
+++ run_sigma _test.py
@@ -19,7 +19,7 @@ base_log_name = 'sigma_'
 # # Run the test for different sigma values
 for j in range(number_of_equal_runs):
     # round to 2 decimals
-    for i in np.arange(0.05, 0.41, 0.05):
+    for i in np.arange(0.05, 0.41, 0.025):
         sigma = round(i, 2)
         log_name = base_log_name + str(sigma) + '_number_' + str(j)
         # Create the command
