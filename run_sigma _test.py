10583da29f4479e73b7e2f985b381675932fd44d
ThotKampOpstrup <thoropstrup@gmail.com>
2023-12-29 09:36:36 +0100

    Automated push: Test for sigma

--- run_sigma _test.py
+++ run_sigma _test.py
@@ -33,17 +33,14 @@ for i in np.arange(0.125, 0.41, 0.025):
     # print('sigma = ' + str_sigma + ' number ' + str(1))
     print(log_name)
     command = [python_interpreter, path+'iem-ppo.py', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
-    # command = [python_interpreter, path+'iem-ppo.py']
 
-    # command = [python_interpreter, path+'test.py']
-
-    # subprocess.run(command)
+    subprocess.run(command)
     
     #push to git
-    # subprocess.run(['git', 'pull'])
-    # subprocess.run(['git', 'add', '-A'])
-    # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
-    # subprocess.run(['git', 'push'])
+    subprocess.run(['git', 'pull'])
+    subprocess.run(['git', 'add', '-A'])
+    subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+    subprocess.run(['git', 'push'])
 
 
 
@@ -62,17 +59,14 @@ for j in range(2,number_of_equal_runs):
         # print('sigma = ' + str_sigma + ' number ' + str(j))
         print(log_name)
         command = [python_interpreter, path+'iem-ppo.py', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
-        # command = [python_interpreter, path+'iem-ppo.py']
 
-        # command = [python_interpreter, path+'test.py']
-
-        # subprocess.run(command)
+        subprocess.run(command)
         
         #push to git
-        # subprocess.run(['git', 'pull'])
-        # subprocess.run(['git', 'add', '-A'])
-        # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
-        # subprocess.run(['git', 'push'])
+        subprocess.run(['git', 'pull'])
+        subprocess.run(['git', 'add', '-A'])
+        subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+        subprocess.run(['git', 'push'])
 
 
 
