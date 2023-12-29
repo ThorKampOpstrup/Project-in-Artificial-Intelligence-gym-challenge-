e751989f9deab8abc88a32231ccd05f49d64c70e
ThotKampOpstrup <thoropstrup@gmail.com>
2023-12-29 09:34:07 +0100

    Automated push: Test for sigma

--- run_sigma _test.py
+++ run_sigma _test.py
@@ -18,7 +18,7 @@ data_path = 'data'
 subdir = 'sigma_test_theta' + theta
 base_log_name = 'sigma_'
 # # Run the test for different sigma values
-for j in range(number_of_equal_runs):
+for j in range(2,number_of_equal_runs):
     # round to 2 decimals
     for i in np.arange(0.05, 0.41, 0.025):
         sigma = round(i, 3)
@@ -36,13 +36,44 @@ for j in range(number_of_equal_runs):
 
         # command = [python_interpreter, path+'test.py']
 
-        subprocess.run(command)
+        # subprocess.run(command)
         
-        #push to git
-        subprocess.run(['git', 'pull'])
-        subprocess.run(['git', 'add', '-A'])
-        subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
-        subprocess.run(['git', 'push'])
+        # #push to git
+        # subprocess.run(['git', 'pull'])
+        # subprocess.run(['git', 'add', '-A'])
+        # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+        # subprocess.run(['git', 'push'])
+
 
 
 # ['/home/thops19/Documents/9semester/PPO-for-Beginners/venv/bin/python', '/home/thops19/Documents/9semester/Project-in-Artificial-Intelligence-gym-challenge-/iem-ppo.py', '--path', 'data', '--subdir', 'sigma_test', '--log_name', 'sigma_0.050_number_0', '--it', 150, '--n_steps 1024 --sigma  0.05 --theta 0.5
+
+
+
+
+###DELETE THIS AND CHANGE RANGE TO 0 TO RUMBER FO RUNS
+
+# round to 2 decimals
+for i in np.arange(0.125, 0.41, 0.025):
+    sigma = round(i, 3)
+    str_sigma = str(sigma) #add so 3 decimals are used
+    while 1:
+        if len(str_sigma) == 5:
+            break
+        str_sigma += '0'
+    log_name = base_log_name + str_sigma + '_number_' + str(1)
+    # Create the command
+    print('sigma = ' + str_sigma + ' number ' + str(1))
+    print(log_name)
+    command = [python_interpreter, path+'iem-ppo.py', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
+    # command = [python_interpreter, path+'iem-ppo.py']
+
+    # command = [python_interpreter, path+'test.py']
+
+    # subprocess.run(command)
+    
+    # #push to git
+    # subprocess.run(['git', 'pull'])
+    # subprocess.run(['git', 'add', '-A'])
+    # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+    # subprocess.run(['git', 'push'])
