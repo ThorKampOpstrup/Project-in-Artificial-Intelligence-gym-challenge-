656d2298e0cbddb38253f3436586fc3a82b4f7dc
ThotKampOpstrup <thoropstrup@gmail.com>
2023-12-30 19:05:38 +0100

    Automated push: Test for sigma

--- run_sigma _test.py
+++ run_sigma _test.py
@@ -18,12 +18,6 @@ data_path = 'data'
 subdir = 'TRPO_sigma_test_theta_' + theta
 base_log_name = 'TRPO_sigma_'
 
-log_name = base_log_name + 'test' + '_number_' + str(0)
-command = [python_interpreter, path+'iem-ppo.py', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(0.01), '--theta', str(theta)]
-
-subprocess.run(command)
-
-
 sigma_test =[0.01, 0.02, 0.05, 0.1, 0.2]
 
 # # Run the test for different sigma values
@@ -42,13 +36,13 @@ for j in range(0,number_of_equal_runs):
         print(log_name)
         command = [python_interpreter, path+'iem-ppo.py', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
 
-        subprocess.run(command)
+        # subprocess.run(command)
         
-        #push to git
-        subprocess.run(['git', 'pull'])
-        subprocess.run(['git', 'add', '-A'])
-        subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
-        subprocess.run(['git', 'push'])
+        # #push to git
+        # subprocess.run(['git', 'pull'])
+        # subprocess.run(['git', 'add', '-A'])
+        # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+        # subprocess.run(['git', 'push'])
 
 
 
