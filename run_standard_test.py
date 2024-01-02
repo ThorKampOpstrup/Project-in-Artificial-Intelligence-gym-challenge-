84e40c59acdbd3a9310ec9a048f5aa2249f08d54
ThotKampOpstrup <thoropstrup@gmail.com>
2024-01-02 16:52:52 +0100

    Automated push: Test for sigma

--- run_standard_test.py
+++ run_standard_test.py
@@ -16,37 +16,37 @@ python_interpreter = '/home/thops19/Documents/9semester/PPO-for-Beginners/venv/b
 path = '/home/thops19/Documents/9semester/Project-in-Artificial-Intelligence-gym-challenge-/'
 data_path = 'data'
 
-subdir = 'PPO_standard_test'
+subdir = 'standard_test'
 base_log_name = 'PPO_standard'
-# # Run the test for different sigma values
+
+# Run the test for different sigma values
 for j in range(0,number_of_equal_runs):
     log_name = base_log_name + '_number_' + str(j)
-    print(log_name)
+    print('subdir: ' + subdir + ' log_name: ' + log_name)
     command = [python_interpreter, path+'main.py', '--use_ppo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
 
-    subprocess.run(command)
+    # subprocess.run(command)
     
-    # push to git
-    subprocess.run(['git', 'pull'])
-    subprocess.run(['git', 'add', '-A'])
-    subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
-    subprocess.run(['git', 'push'])
-
-subdir = 'TRPO_standard_test'
+    # # push to git
+    # subprocess.run(['git', 'pull'])
+    # subprocess.run(['git', 'add', '-A'])
+    # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+    # subprocess.run(['git', 'push'])
+    
 base_log_name = 'TRPO_standard'
 
 for j in range(0,number_of_equal_runs):
     log_name = base_log_name + '_number_' + str(j)
-    print(log_name)
+    print('subdir: ' + subdir + ' log_name: ' + log_name)
     command = [python_interpreter, path+'main.py', '--use_ppo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
 
-    subprocess.run(command)
+    # subprocess.run(command)
     
-    # push to git
-    subprocess.run(['git', 'pull'])
-    subprocess.run(['git', 'add', '-A'])
-    subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
-    subprocess.run(['git', 'push'])
+    # # push to git
+    # subprocess.run(['git', 'pull'])
+    # subprocess.run(['git', 'add', '-A'])
+    # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+    # subprocess.run(['git', 'push'])
 
 
 
