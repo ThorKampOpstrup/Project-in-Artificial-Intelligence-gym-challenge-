c64203692b3e5c725e0a85d5a143a7cd5e33190a
ThotKampOpstrup <thoropstrup@gmail.com>
2024-01-03 09:13:15 +0100

    Automated push: Test for sigma

--- run_standard_test.py
+++ run_standard_test.py
@@ -19,26 +19,26 @@ data_path = 'data'
 subdir = 'standard_test'
 base_log_name = 'PPO_standard'
 
-# Run the test for different sigma values
+# # Run the test for different sigma values
+# for j in range(0,number_of_equal_runs):
+#     log_name = base_log_name + '_number_' + str(j)
+#     print('subdir: ' + subdir + ' log_name: ' + log_name)
+#     command = [python_interpreter, path+'main.py', '--use_ppo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
+
+#     subprocess.run(command)
+    
+#     # push to git
+#     subprocess.run(['git', 'pull'])
+#     subprocess.run(['git', 'add', '-A'])
+#     subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
+#     subprocess.run(['git', 'push'])
+    
+# base_log_name = 'TRPO_standard'
+
 for j in range(0,number_of_equal_runs):
     log_name = base_log_name + '_number_' + str(j)
     print('subdir: ' + subdir + ' log_name: ' + log_name)
-    command = [python_interpreter, path+'main.py', '--use_ppo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
-
-    subprocess.run(command)
-    
-    # push to git
-    subprocess.run(['git', 'pull'])
-    subprocess.run(['git', 'add', '-A'])
-    subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma = ' + str(sigma) + ' number ' + str(j)])
-    subprocess.run(['git', 'push'])
-    
-base_log_name = 'TRPO_standard'
-
-for j in range(0,number_of_equal_runs):
-    log_name = base_log_name + '_number_' + str(j)
-    print('subdir: ' + subdir + ' log_name: ' + log_name)
-    command = [python_interpreter, path+'main.py', '--use_ppo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
+    command = [python_interpreter, path+'main.py', '--use_trpo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta)]
 
     subprocess.run(command)
     
