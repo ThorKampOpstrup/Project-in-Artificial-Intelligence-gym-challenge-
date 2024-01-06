cffe9d98a5585b5771711913aad6c3cf9457ed66
ThotKampOpstrup <thoropstrup@gmail.com>
2024-01-06 13:54:19 +0100

    Automated push: Test for combined

--- run_ppo_re3+ou_test.py
+++ run_ppo_re3+ou_test.py
@@ -21,15 +21,15 @@ data_path = 'data'
 subdir = 'PPO_Re3_OU_test'
 base_log_name = 'PPO_k_' + k + '_sigma_' + sigma + '_theta_' + theta
 
-for j in range(0,number_of_equal_runs):
+for j in range(4,number_of_equal_runs):
     log_name = base_log_name + '_number_' + str(j)
     print('subdir: ' + subdir + ' log_name: ' + log_name)
     command = [python_interpreter, path+'main.py', '--use_ppo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta), '--re3_k', str(k)]
 
-    subprocess.run(command)
+    # subprocess.run(command)
     
-    # push to git
-    subprocess.run(['git', 'pull'])
-    subprocess.run(['git', 'add', '-A'])
-    subprocess.run(['git', 'commit', '-m', 'Automated push: Test for combined'])
-    subprocess.run(['git', 'push'])
+    # # push to git
+    # subprocess.run(['git', 'pull'])
+    # subprocess.run(['git', 'add', '-A'])
+    # subprocess.run(['git', 'commit', '-m', 'Automated push: Test for combined'])
+    # subprocess.run(['git', 'push'])
