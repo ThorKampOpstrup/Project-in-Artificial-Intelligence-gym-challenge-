9677edcb2b3317cb465e2e41115723c5220f0a4e
ThotKampOpstrup <thoropstrup@gmail.com>
2024-01-06 10:21:00 +0100

    Automated push: Test for combined

--- run_trpo_re3+ou_test.py
+++ run_trpo_re3+ou_test.py
@@ -3,7 +3,7 @@ import numpy as np
 
 subprocess.run(['git', 'pull'])
 subprocess.run(['git', 'add', '-A'])
-subprocess.run(['git', 'commit', '-m', 'Automated push: Test for combined'])
+subprocess.run(['git', 'commit', '-m', 'Automated push: Test for sigma'])
 subprocess.run(['git', 'push'])
 
 theta = str(5)
@@ -18,13 +18,13 @@ python_interpreter = '/home/thops19/Documents/9semester/PPO-for-Beginners/venv/b
 path = '/home/thops19/Documents/9semester/Project-in-Artificial-Intelligence-gym-challenge-/'
 data_path = 'data'
 
-subdir = 'PPO_Re3_OU_test'
-base_log_name = 'PPO_k_' + k + '_sigma_' + sigma + '_theta_' + theta
+subdir = 'TRPO_Re3_OU_test'
+base_log_name = 'TRPO_k_' + k + '_sigma_' + sigma + '_theta_' + theta
 
 for j in range(0,number_of_equal_runs):
     log_name = base_log_name + '_number_' + str(j)
     print('subdir: ' + subdir + ' log_name: ' + log_name)
-    command = [python_interpreter, path+'main.py', '--use_ppo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta), '--re3_k', str(k)]
+    command = [python_interpreter, path+'main.py', '--use_trpo', 'True', '--path', data_path,'--subdir', subdir, '--log_name', log_name, '--it', iterations, '--n_steps', '1024', '--sigma', str(sigma), '--theta', str(theta), '--re3_k', str(k)]
 
     subprocess.run(command)
     
