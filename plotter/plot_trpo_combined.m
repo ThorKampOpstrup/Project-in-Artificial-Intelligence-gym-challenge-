4f1b5d4fddafb0ba5a4a3910f4f0e54359dcb9ac
ThotKampOpstrup <thoropstrup@gmail.com>
2024-01-07 16:03:19 +0100

    Automated push: Test for combined

--- plotter/plot_trpo_combined.m
+++ plotter/plot_trpo_combined.m
@@ -1,3 +1,4 @@
+close all;
 addpath("plotter/");
 
 load_avg_standard_trpo;
@@ -8,17 +9,19 @@ TRPO_step = 1;
 TRPO_rollout_ep_len_mean = 2;
 TRPO_rollout_ep_rew_mean = 3;
 TRPO_time_fps = 4;
-TRPO_train_fitness_score = 5;
-TRPO_train_mean_evaluation_length = 6;
-TRPO_train_mean_evaluation_reward = 7;
-TRPO_train_real_mean_ep_len = 8;
-TRPO_train_explained_variance = 9;
-TRPO_train_is_line_search_success = 10;
-TRPO_train_kl_divergence_loss = 11;
-TRPO_train_learning_rate = 12;
-TRPO_train_policy_objective = 13;
-TRPO_train_std = 14;
-TRPO_train_value_loss = 15;
+TRPO_train_beta = 5;
+TRPO_train_fitness_score = 6;
+TRPO_train_mean_episodic_Re3_reward = 7;
+TRPO_train_mean_evaluation_length = 8;
+TRPO_train_mean_evaluation_reward = 9;
+TRPO_train_real_mean_ep_len = 10;
+TRPO_train_explained_variance = 11;
+TRPO_train_is_line_search_success = 12;
+TRPO_train_kl_divergence_loss = 13;
+TRPO_train_learning_rate = 14;
+TRPO_train_policy_objective = 15;
+TRPO_train_std = 16;
+TRPO_train_value_loss = 17;
 
 path = "data_cpy/csv/TRPO_Re3_OU_test/";
 
@@ -51,7 +54,7 @@ TRPO_Re3_OU_test_mean =(run1 + run2 + run3 + run4 + run5 + run6 + run7 + run8 + 
 figure(1);
 hold on;
 plot(iterations_vec, TRPO_Re3_OU_test_mean(:, TRPO_train_mean_evaluation_reward), 'LineWidth', 1);
-plot(iterations_vec, TRPO_standad_avg(:, trpo_train_mean_evaluation_reward), 'LineWidth', 1);
+plot(iterations_vec, trpo_standad_avg(:, trpo_train_mean_evaluation_reward), 'LineWidth', 1);
 legend("TRPO Re3 OU test", "TRPO standard");
 xlabel("Iterations");
 ylabel("Mean evaluation reward");
@@ -80,10 +83,17 @@ run19_first_300 = return_it_where_val_is_reached(run19, 300, TRPO_train_mean_eva
 run20_first_300 = return_it_where_val_is_reached(run20, 300, TRPO_train_mean_evaluation_reward);
 
 TRPO_Re3_OU_first_300 = [run1_first_300, run2_first_300, run3_first_300, run4_first_300, run5_first_300, run6_first_300, run7_first_300, run8_first_300, run9_first_300, run10_first_300, run11_first_300, run12_first_300, run13_first_300, run14_first_300, run15_first_300, run16_first_300, run17_first_300, run18_first_300, run19_first_300, run20_first_300];
-TRPO_Re3_OU_first_300 = TRPO_Re3_OU_first_300(TRPO_Re3_OU_first_300 ~= rows+1);
+%TRPO_Re3_OU_first_300 = TRPO_Re3_OU_first_300(TRPO_Re3_OU_first_300 ~= rows+1);
 
 TRPO_Re3_OU_first_300_mean = mean(TRPO_Re3_OU_first_300);
 TRPO_Re3_OU_first_300_lowest = min(TRPO_Re3_OU_first_300);
 
+figure(2);
+hold on;
+xticks_vec = ["TRPO standard", "TRPO Re3 OU test"];
+bar(xticks_vec, [trpo_first_300_mean, trpo_first_300_lowest; TRPO_Re3_OU_first_300_mean, TRPO_Re3_OU_first_300_lowest]);
 
-
+legend("Mean", "Lowest");
+title("TRPO standard vs TRPO Re3 OU test");
+ylabel("Iterations");
+hold off;
